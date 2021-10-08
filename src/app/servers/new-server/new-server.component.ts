f3c45e14eb6104a138fb92776020118d78222f34
Richard Thompson <rich10tom@gmail.com>
2021-10-07 20:09:28 -0500

    In class exercise

---
+++ src/app/servers/new-server/new-server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-server',
+  templateUrl: './new-server.component.html',
+  styleUrls: ['./new-server.component.css']
+})
+export class NewServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
